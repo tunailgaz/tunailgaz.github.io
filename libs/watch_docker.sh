954690e0d801e7f490ffd6548e4635e1a4165b99
Tuna <tunailgaz@gmail.com>
2023-06-12 13:54:37 +0300

    what is this repo

--- libs/watch_docker.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker events --format '{{.From}}' --filter 'event=start' | while read line; do  if [[ ${line} = "gogoanime_gogoanime" ||  ${line} = "anime_anime" ]];then echo " container ${line} started $(date), will restart nginx after 10secs "; sleep 10s ; docker container restart nginx; fi ; done
